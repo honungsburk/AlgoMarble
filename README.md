8f92ab8952bf913502afde44d40905a143e7e25e
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2021-09-07 16:36:46 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Brilliant Improbability (Shader NFTs on Cardano)
+
+'Brilliant Improbability' is a generative art project by Frank Hampus Weslien.
+You are free to download, modify, and share the code. But remember: if you want
+to use it create your own NFT:s make sure that you've modified it enough that it
+has become something new/novel.
+
+## Dependencies
+
+It uses python 3 (no libraries) and glslViewer. They need to be installed for the
+program to run.
+
+## Basics of how it works
+
+### Overview
+
+The basic idea is to stack a bunch of noise on top of each other and then map that
+noise into pretty colors. If you want to know how I recommend checking out the code in the
+`brilliant-improbability.frag` shader.
+
+Now, shaders are deterministic so noise has to be added from outside:
+hence there is a python script. It selects parameters for the shaders based on a seed
+(in the original collection I used the seeds 0-511). I've saved the seeds, but 
+also the parameters values the seeds generated. You will find what values a specific
+NFT has by looking at its metadata. 
+
+Q: But why did you use glslViewer???  
+A: It was the first approach that didn't choke when I increased the 
+image resolution to 6000 by 4000 pixels on my shitty laptop. 
+I want you to be able to recreate the NFTs in high resolution if you need to.
+
+### Files
+
+#### 'brilliant-improbability.frag'
+
+This is the shader that ultimately generates the art and is written in OpenGL.
+If you've never encountered OpenGl before the best way to think about it describes 
+a (deterministic) function that gets applied to every pixel. Shaders runs on your 
+graphics cards and are really fast which is why I'm using it.
+
+#### 'input.csv'
+
+This file a list of commands (formatted as CSV because that is what glslViewer expects)
+that can be modified to easily recreate any of the artworks in the 'Brilliant 
+Improbability' collection.
+
+Each line in the file is a command to glslViewer. 
+The first series of command all sets input parameters to the shader (usually called uniforms)
+then asks glslViewer to perform a screenCapture and save it as a PNG image and then exit.
+
+#### 'script.py'
+
+A script that can recreate any number of artworks in the 'Brilliant 
+Improbability' collection. It was also used to create the original collection that
+was minted as NFTs in September of 2021.
+
+## How to recreate
+
+### Option 1 (No Python)
+
+If you have no interest in installing python you can recreate one of the artworks
+by copying the parameters in your CNFT (Cardano Non-Fungible Token) transaction 
+metadata into the `input.csv` file. Simply replace the values in the file
+with your values and then run:
+
+```bash
+cat input.csv | glslViewer brilliant-improbability.frag -w 1200 -h 800 --headless
+```
+
+It will generate a PNG image of 1200 by 800 pixels, you can easily modify the command
+to generate an output in any size that you'd like.
+
+### Option 2 (With Python)
+
+The basic command is:
+
+```bash
+python3 script.py --width 1200 --height 800 --start 0 --end 512  
+```
+
+You can obviously modify it to your liking...
+If you need any help you can use the `-h` command:
+
+```bash
+python3 script.py -h
+```
---
+++ script.py
@@ -0,0 +1,118 @@
+
+import subprocess
+import argparse
+import random
+import os
+import json
+
+# glslVeiwer command to take a screenshot
+# This command is used to create images
+def screenshotCommand(name):
+    return ['screenshot', name + '.png']
+
+# Exit glslViewer
+def exitCommand():
+    return ['exit']
+
+# Commands are piped as CSV to glslViewer and since our demands are very small
+# we implement it ourselves rather then relying on any library
+def commandsToString(commands):
+    commands_s = ''
+    for command in commands:
+        is_first = True
+        for part in command:
+            if is_first:
+                commands_s += part
+                is_first = False
+            else:
+                commands_s +=  ', ' + part
+        commands_s += '\n'
+    
+    return commands_s
+
+# Take the dictionary and turn it into a list of lists
+# that is a simple representation of a series of commands
+def to_uniform_commands(uniforms):
+    commands = []
+    for uniform in uniforms:
+        command = [uniform]
+        params = uniforms[uniform]
+        if isinstance(params, float):
+            command.append(str(params))
+        else: 
+            for param in params:
+                command.append(str(param))
+
+        commands.append(command)
+
+    return commands
+
+def create_uniforms(seed):
+    random.seed(a=seed, version=2)
+
+
+    return {
+        'u_numOctaves' : random.uniform(8, 16),
+        'u_zoom' : random.uniform(1.0, 2.5) if random.random() else random.uniform(0.2, 1.5),
+        'u_cc' : (random.uniform(10, 20), random.uniform(10, 20), random.uniform(10, 20)), 
+        'u_dd': (random.random(), random.random(), random.random()), 
+        'u_q_h': (random.uniform(0.7, 1.3), random.uniform(0.7, 1.3)), 
+        'u_r_h': (random.uniform(0.7, 1.3), random.uniform(0.7, 1.3)),
+        'u_pattern_h': random.uniform(0.8, 1.2),
+        'u_center_point': (random.random(), random.random()),
+        'u_pixel_distance_choice': random.random(),
+        'u_interpolation_choice': random.uniform(0.0, 3.0),
+        'u_q_fbm_displace_1': (random.uniform(0, 20), random.uniform(0, 20)),
+        'u_q_fbm_displace_2': (random.uniform(0, 20), random.uniform(0, 20)),
+        'u_r_fbm_displace_1': (random.uniform(0, 20), random.uniform(0, 20)),
+        'u_r_fbm_displace_2': (random.uniform(0, 20), random.uniform(0, 20)),
+        'u_color_speed': random.uniform(0.5, 1.0),
+    }
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Generate PNG images from the Brilliant Improbability NFT Series.')
+    parser.add_argument('--width', metavar='PIXELS', type=int, default=1200,
+                        help='the width of the image in pixels')
+    parser.add_argument('--height', metavar='PIXELS', type=int, default=800,
+                        help='the width of the image in pixels')
+    parser.add_argument('--start', metavar='INT', type=int, default=0,
+                        help='starting seed when generating images (inclusive)')
+    parser.add_argument('--end', metavar='INT', type=int, default=512,
+                        help='ending seed when generating images (non-inclusive)')
+
+
+    args = parser.parse_args()
+
+    shaderPath = 'brilliant-improbability.frag'
+
+    glslCommand = ' '.join(['glslViewer', shaderPath, '--headless', '-w', str(args.width), '-h', str(args.height)])
+
+    print('Running command:')
+    print(glslCommand)
+
+
+    for seed in range(args.start, args.end):
+        glslViewer = subprocess.Popen( glslCommand
+                                    , stdin=subprocess.PIPE
+                                    , stdout=subprocess.PIPE
+                                    , encoding='utf8'
+                                    , shell=True)
+        print(seed)
+        uniforms = create_uniforms(seed)
+        
+        path = str(seed)
+
+        # Create commands to send to glslViewer
+        uniform_commands = to_uniform_commands(uniforms)
+        uniform_commands.append(screenshotCommand(path))
+        uniform_commands.append(exitCommand())
+        commands_as_string = commandsToString(uniform_commands)
+        print(commands_as_string)
+        # Will block until all images has been rendered
+        glslViewer.communicate(commands_as_string)
+
+        with open(path + ".json", "w") as json_file:
+            json.dump(uniforms, json_file, indent = 4) 
+
+    
